5c61a4efe8d403275fe8801946043a1ac9726336
UfukSenkal <67637340+UfukSenkal@users.noreply.github.com>
2021-11-19 01:26:41 +0300

    +create design level with one button in editor with setup scene

--- Assets/Scripts/Editor/CreateCarEditor.cs
+++ Assets/Scripts/Editor/CreateCarEditor.cs
@@ -6,20 +6,56 @@ using UnityEngine;
 
 public class CreateCarEditor : Editor
 {
+    [MenuItem("Car Game/Setup Scene")]
+    static void SetupScene()
+    {
+      
+        GameObject managers = Resources.Load<GameObject>("Prefabs/ManagerPoolBase");
+        if (FindObjectOfType<ManagerPoolBase>() == null)
+        {
+            Instantiate(managers);
+           
+        }
+
+        GameObject camera = Resources.Load<GameObject>("Prefabs/MainCamera");
+        if (FindObjectOfType<Camera>() == null)
+        {
+            Instantiate(camera);
+        }
+        else
+        {
+            Camera mainCam = FindObjectOfType<Camera>();
+            mainCam.orthographic = true;
+            mainCam.transform.position = camera.transform.position;
+            mainCam.transform.rotation = camera.transform.rotation;
+        }
+
+        GameObject carBase = Resources.Load<GameObject>("Prefabs/CarPoolBase");
+        if (FindObjectOfType<CarParentBase>() == null)
+        {
+            Instantiate(carBase);
+        }
+
+        GameObject obstacleBase = Resources.Load<GameObject>("Prefabs/ObstaclePoolBase");
+        if (FindObjectOfType<ObstacleParentBase>() == null)
+        {
+            Instantiate(obstacleBase);
+        }
+    }
 
     [MenuItem("Car Game/Create Car")]
     static void CreateCar()
     {
-        GameObject cars = FindObjectOfType<CarParentBase>().gameObject;
+        
         GameObject carPackage = Resources.Load<GameObject>("Prefabs/CarBase");
 
-        if (cars == null)
+        if (FindObjectOfType<CarParentBase>() == null)
         {
             Debug.LogError("Need to Setup Scene  --- Cars GameObject is Missing");
         }
         else
         {
-           
+            GameObject cars = FindObjectOfType<CarParentBase>().gameObject;
             Instantiate(carPackage, cars.transform);
         }
 
@@ -30,15 +66,16 @@ public class CreateCarEditor : Editor
     [MenuItem("Car Game/Create Obstacle")]
     static void CreateObstacle()
     {
-        GameObject obstacles = FindObjectOfType<ObstacleParentBase>().gameObject;
+       
         GameObject carPackage = Resources.Load<GameObject>("Prefabs/Obstacle");
 
-        if (obstacles == null)
+        if (FindObjectOfType<ObstacleParentBase>() == null)
         {
             Debug.LogError("Need to Setup Scene  --- Obstacle GameObject is Missing");
         }
         else
         {
+            GameObject obstacles = FindObjectOfType<ObstacleParentBase>().gameObject;
             Instantiate(carPackage, obstacles.transform);
         }
 
