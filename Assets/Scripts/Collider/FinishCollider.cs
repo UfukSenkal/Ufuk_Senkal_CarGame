74abef416c2537cd872ea15d3a77bbe472625bb8
UfukSenkal <67637340+UfukSenkal@users.noreply.github.com>
2021-11-18 03:43:04 +0300

    +added collider for finish

---
+++ Assets/Scripts/Collider/FinishCollider.cs
@@ -0,0 +1,25 @@
+using CarGame.Car.Movement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishCollider : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        CarMovementController car = other.gameObject.GetComponent<CarMovementController>();
+        if (car.isActive())
+        {
+
+            car.ResetPos();
+            GameManager.Instance.CurrentGameState = GameManager.GameState.WaitingInput;
+            CarManager.Instance.carState = CarManager.CarState.Waiting;
+            CarManager.Instance.ChangeActiveCar();
+        }
+        else
+        {
+            car.Park();
+        }
+    }
+
+}
