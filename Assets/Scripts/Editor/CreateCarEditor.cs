86302496d2309134b8cccdfdb9814283542f6753
UfukSenkal <67637340+UfukSenkal@users.noreply.github.com>
2021-11-18 17:44:37 +0300

    +added editor button for level designer now can instantiate prefabs with one click

---
+++ Assets/Scripts/Editor/CreateCarEditor.cs
@@ -0,0 +1,47 @@
+﻿using CarGame.Car;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CreateCarEditor : Editor
+{
+
+    [MenuItem("Car Game/Create Car")]
+    static void CreateCar()
+    {
+        GameObject carPackage = Resources.Load<GameObject>("Prefabs/CarBase");
+
+        if (FindObjectOfType<CarParentBase>() == null)
+        {
+            Debug.LogError("Need to Setup Scene  --- Cars GameObject is Missing");
+        }
+        else
+        {
+            GameObject cars = FindObjectOfType<CarParentBase>().gameObject;
+            Instantiate(carPackage, cars.transform);
+        }
+
+        
+
+    }
+
+    [MenuItem("Car Game/Create Obstacle")]
+    static void CreateObstacle()
+    {
+        GameObject obstacles = FindObjectOfType<ObstacleParentBase>().gameObject;
+        GameObject carPackage = Resources.Load<GameObject>("Prefabs/Obstacle");
+
+        if (obstacles == null)
+        {
+            Debug.LogError("Need to Setup Scene  --- Obstacle GameObject is Missing");
+        }
+        else
+        {
+            Instantiate(carPackage, obstacles.transform);
+        }
+
+        
+
+    }
+}
